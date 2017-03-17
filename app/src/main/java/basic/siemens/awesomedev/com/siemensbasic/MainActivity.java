4f952a8de2cab5dccae8ac9a68e39bf6b3fd7372
Sparsh Bansal <sparsh.bansal17895@gmail.com>
2017-03-17 10:55:26 +0530

    Cleanup Code

--- app/src/main/java/basic/siemens/awesomedev/com/siemensbasic/MainActivity.java
+++ app/src/main/java/basic/siemens/awesomedev/com/siemensbasic/MainActivity.java
@@ -1,24 +1,16 @@
 package basic.siemens.awesomedev.com.siemensbasic;
 
-import android.accounts.AccountManager;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,16 +18,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
-
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.drive.DriveApi;
 import com.google.android.gms.drive.DriveFolder;
-import com.google.android.gms.drive.Metadata;
 import com.google.android.gms.drive.MetadataChangeSet;
-import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.util.ExponentialBackOff;
 import com.google.api.services.drive.Drive;
@@ -50,11 +38,6 @@ import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
-import java.util.jar.Manifest;
-
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener, /*EasyPermissions.PermissionCallbacks,*/
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
@@ -74,10 +57,6 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
 
     // Request Code for Camera Intent
     private static final int REQUEST_CAPTURE_IMAGE = 1000;
-    private static final int REQUEST_ACCOUNT_PICKER = 1001;
-    private static final int REQUEST_AUTHORIZATION = 1002;
-    private static final int REQUEST_GOOGLE_PLAY_SERVICES = 1003;
-    private static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1004;
     private static final int REQUEST_RESOLVE_CONNECTION = 1005;
 
     // Path for the current photo
@@ -140,37 +119,8 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         switch (requestCode) {
             case REQUEST_CAPTURE_IMAGE:
                 Toast.makeText(this, "Captured the image", Toast.LENGTH_SHORT).show();
-//                getResultsFromApi();
                 saveToDrive();
                 break;
-/*
-            case REQUEST_GOOGLE_PLAY_SERVICES:
-                if (resultCode != RESULT_OK) {
-                    Toast.makeText(this, "This app requires google play services", Toast.LENGTH_SHORT).show();
-                } else {
-                    // Get the results from api
-                }
-                break;
-
-            case REQUEST_ACCOUNT_PICKER:
-                if (resultCode == RESULT_OK && data != null && data.getExtras() != null) {
-                    String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-                    if (accountName != null) {
-                        SharedPreferences settings =
-                                getPreferences(Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = settings.edit();
-                        editor.putString(PREF_ACCOUNT_NAME, accountName);
-                        editor.apply();
-                        mCredentials.setSelectedAccountName(accountName);
-                        getResultsFromApi();
-                    }
-                }
-                break;
-            case REQUEST_AUTHORIZATION:
-                if (resultCode == RESULT_OK) {
-                    getResultsFromApi();
-                }
-                break;*/
 
             case REQUEST_RESOLVE_CONNECTION:
                 if (resultCode == RESULT_OK) {
@@ -179,26 +129,6 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
                 break;
         }
     }
-
-/*
-    private void getResultsFromApi() {
-        if (!isGooglePlayServicesAvailable()) {
-            acquireGooglePlayServices();
-        } else if (mCredentials.getSelectedAccountName() == null) {
-            chooseAccount();
-        } else if (!isDeviceOnline()) {
-            Toast.makeText(this, "No Network Available", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Good to Go!", Toast.LENGTH_SHORT).show();
-            saveToDrive();
-        }
-    }
-
-    public void authorize(){
-
-    }
-
-*/
 
     private void saveToDrive() {
         com.google.android.gms.drive.Drive.DriveApi.newDriveContents(mClient).setResultCallback(new ResultCallback<DriveApi.DriveContentsResult>() {
@@ -248,6 +178,7 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         public void onResult(@NonNull DriveFolder.DriveFileResult driveFileResult) {
             if (driveFileResult.getStatus().isSuccess()){
                 Toast.makeText(MainActivity.this, "File Successfully Created!!", Toast.LENGTH_SHORT).show();
+                
                 return;
             }
             else{
@@ -256,28 +187,6 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
             }
         }
     };
-/*
-
-
-    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
-    private void chooseAccount() {
-        if (EasyPermissions.hasPermissions(this, android.Manifest.permission.GET_ACCOUNTS)) {
-            String accountName = getPreferences(Context.MODE_PRIVATE).getString(PREF_ACCOUNT_NAME, null);
-            if (accountName != null) {
-                mCredentials.setSelectedAccountName(accountName);
-
-                getResultsFromApi();
-
-            } else {
-                startActivityForResult(mCredentials.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
-            }
-        } else {
-            EasyPermissions.requestPermissions(this, "The app needs to access your google account",
-                    REQUEST_PERMISSION_GET_ACCOUNTS, android.Manifest.permission.GET_ACCOUNTS);
-        }
-    }
-
-*/
 
     private void dispatchTakePictureIntent() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -314,53 +223,6 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         return image;
 
     }
-/*
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
-    }
-
-    @Override
-    public void onPermissionsGranted(int requestCode, List<String> perms) {
-
-    }
-
-    @Override
-    public void onPermissionsDenied(int requestCode, List<String> perms) {
-
-    }
-
-
-    private boolean isDeviceOnline() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(connectivityManager.getActiveNetwork());
-        return (networkInfo != null && networkInfo.isConnected());
-    }
-
-    private boolean isGooglePlayServicesAvailable() {
-        GoogleApiAvailability mAvailability = GoogleApiAvailability.getInstance();
-        int availabilityStatusCode = mAvailability.isGooglePlayServicesAvailable(this);
-
-        return availabilityStatusCode == ConnectionResult.SUCCESS;
-
-    }
-
-    private void acquireGooglePlayServices() {
-
-        GoogleApiAvailability mAvailability = GoogleApiAvailability.getInstance();
-        int availabilityStatusCode = mAvailability.isGooglePlayServicesAvailable(this);
-
-        if (mAvailability.isUserResolvableError(availabilityStatusCode)) {
-            showGooglePlayServicesAvailabilityErrorDialog(availabilityStatusCode);
-        }
-    }
-
-    private void showGooglePlayServicesAvailabilityErrorDialog(int availabilityStatusCode) {
-        GoogleApiAvailability mAvailability = GoogleApiAvailability.getInstance();
-        Dialog dialog = mAvailability.getErrorDialog(this, availabilityStatusCode, REQUEST_GOOGLE_PLAY_SERVICES);
-        dialog.show();
-    }*/
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
